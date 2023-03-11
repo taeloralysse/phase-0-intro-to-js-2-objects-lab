10359f7f8b82ae9ecf9ebb66f4d69bf12b729b72
Taelor Lambert <taeloralysse@gmail.com>
2023-03-10 19:57:14 -0500

    objectsss

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {name: "legalName", address: "streetAddress"}
+function updateEmployeeWithKeyAndValue(employee, name, Sam) {
+    const newEmployee = {...employee}
+    newEmployee[name] = Sam
+
+    return {...employee, [name]: Sam} 
+}
+streetAddress = "11 Broadway"
+function destructivelyUpdateEmployeeWithKeyAndValue(newEmployee, address, streetAddress) {
+    newEmployee[address] = "12 Broadway"
+    return newEmployee
+}
+function deleteFromEmployeeByKey(newEmployee, name, undefined){
+    return {...newEmployee, [name]: undefined}
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name, undefined){
+    employee[name] = undefined
+    return employee
+}
